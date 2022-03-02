7b935b98eedce1105196594e4e07b1a82a031969
chipat <40916066+chpatil@users.noreply.github.com>
2022-03-02 14:26:12 +0000

    Final completion

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ start_flag=True
 no_first_attempt=False
 wrong_answer=False
 score=0
+a={}
+b={}
 retainer={}
 def get_data():
   data=game_data.data
-  return data[random.randint(0,len(data))]
-def get_sentence(a):
-  return "Compare A: "+a["name"]+", a "+a["description"]+", from"+ a["country"]
+  return data[random.randint(0,len(data)-1)]
+def get_sentence(a,object):
+  return f"Compare {object}: "+a['name']+", a "+a['description']+", from "+ a['country']
 def check(decision,a,b):
-    if(a["followers"]>b["followers"]):
+    if(a["follower_count"]>b["follower_count"]):
       higher="a"
     else:
       higher="b"
-      retainer=b
     if(decision==higher):
       return True
     else:
@@ -26,27 +27,23 @@ def check(decision,a,b):
 while(start_flag):
   print(art.logo)
   if(no_first_attempt):
-    print("You're right! Current score: "+score)
+    print("You're right! Current score: "+str(score))
   no_first_attempt=True
-  a=retainer
-  b={}
-  if(a is None):
+  #print(str(a))
+  if(len(a)==0):
     a=get_data()
+  #print(a)
   b=get_data()
-  print(get_sentence(a))
+  #print(b)
+  print(get_sentence(a,"A"))
   print(art.vs)
-  print(get_sentence(b))
-  if(lower(input("Who has more followers? Type 'A' or 'B': ")),a,b):
+  print(get_sentence(b,"B"))
+  if(check(input("Who has more followers? Type 'A' or 'B': ").lower(),a,b)):
     score+=1
     replit.clear()
+    a=b
   else:
     replit.clear()
     print(art.logo)
-    print("Sorry, that's wrong. Final score: "+score)
-    break
-    return
-    
-    
-  
-    
-    
+    print("Sorry, that's wrong. Final score: "+str(score))
+    break 
